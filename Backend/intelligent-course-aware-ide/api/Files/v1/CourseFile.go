e929a81a2623f49b84779041e5d21b5b950185b5
wenxuan <deltaleung3@gmail.com>
2025-04-25 12:41:09 +0800

    GetFile

--- Backend/intelligent-course-aware-ide/api/Files/v1/CourseFile.go
+++ Backend/intelligent-course-aware-ide/api/Files/v1/CourseFile.go
@@ -6,24 +6,27 @@ import (
 )
 
 type UploadLectureFileReq struct {
-	g.Meta    `path:"/api/Files/courseFile/upload" method:"post" tags:"file" summary:"upload CourseFile"`
+	g.Meta    `path:"/api/Files/lectureFile/upload" method:"post" tags:"file" summary:"upload lectureFile"`
 	CourseId  int64             `v:"required" json:"courseId"`
 	LectureId int64             `v:"required" json:"lectureId"`
 	Index     int64             `v:"required" json:"index" dc:"the index of this file in this lecture"`
 	File      *ghttp.UploadFile `v:"required" json:"file"`
 }
 
-type CoverLectureFileReq struct {
-	g.Meta    `path:"/api/Files/courseFile/cover" method:"post" tags:"file" summary:"cover a CourseFile"`
-	CourseId  int64             `v:"required" json:"courseId"`
-	LectureId int64             `v:"required" json:"lectureId"`
-	Index     int64             `v:"required" json:"index" dc:"the index of this file in this lecture"`
-	File      *ghttp.UploadFile `v:"required" json:"file"`
+type UpdateLectureFileReq struct {
+	g.Meta `path:"/api/Files/lectureFile/{FileId}" method:"put" tags:"file" summary:"update a lectureFile"`
+	FileId int64             `v:"required" dc:"file id"`
+	File   *ghttp.UploadFile `v:"required" json:"file"`
 }
 
 type DeleteLectureFileReq struct {
-	g.Meta `path:"/api/Files/courseFile/delete" method:"post" tags:"file" summary:"delete CourseFile"`
-	FileId uint `v:"required" dc:"an unique file id"`
+	g.Meta `path:"/api/Files/lectureFile/{FileId}" method:"delete" tags:"file" summary:"delete lecture File"`
+	FileId int64 `v:"required" dc:"an unique file id"`
+}
+
+type GetLectureFileReq struct {
+	g.Meta `path:"/api/Files/lectureFile/{FileId}" method:"get" tags:"file" summary:"get lecture File"`
+	FileId int64 `v:"required" dc:"an unique file id"`
 }
 
 type UploadLectureFileRes struct {
@@ -31,11 +34,16 @@ type UploadLectureFileRes struct {
 	Id     int64 `json:"id" dc:"file id"`
 }
 
-type CoverLectureFileRes struct {
+type UpdateLectureFileRes struct {
 	Result bool  `json:"result" dc:"is OK or not"`
 	Id     int64 `json:"id" dc:"file id"`
 }
 
 type DeleteLectureFileRes struct {
 	Result bool `json:"result" dc:"is OK or not"`
+}
+
+type GetLectureFileRes struct {
+	Result bool              `json:"result" dc:"is OK or not"`
+	File   *ghttp.UploadFile `json:"file"`
 }
