535bc4ba226228eb4239beffbec9d1ead1b21e6f
wenxuan <deltaleung3@gmail.com>
2025-04-25 12:27:52 +0800

    Change Name

--- Backend/intelligent-course-aware-ide/api/Files/v1/CourseFile.go
+++ Backend/intelligent-course-aware-ide/api/Files/v1/CourseFile.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
-type UploadFileReq struct {
+type UploadLectureFileReq struct {
 	g.Meta    `path:"/api/Files/courseFile/upload" method:"post" tags:"file" summary:"upload CourseFile"`
 	CourseId  int64             `v:"required" json:"courseId"`
 	LectureId int64             `v:"required" json:"lectureId"`
@@ -13,7 +13,7 @@ type UploadFileReq struct {
 	File      *ghttp.UploadFile `v:"required" json:"file"`
 }
 
-type CoverFileReq struct {
+type CoverLectureFileReq struct {
 	g.Meta    `path:"/api/Files/courseFile/cover" method:"post" tags:"file" summary:"cover a CourseFile"`
 	CourseId  int64             `v:"required" json:"courseId"`
 	LectureId int64             `v:"required" json:"lectureId"`
@@ -21,21 +21,21 @@ type CoverFileReq struct {
 	File      *ghttp.UploadFile `v:"required" json:"file"`
 }
 
-type DeleteFileReq struct {
+type DeleteLectureFileReq struct {
 	g.Meta `path:"/api/Files/courseFile/delete" method:"post" tags:"file" summary:"delete CourseFile"`
 	FileId uint `v:"required" dc:"an unique file id"`
 }
 
-type UploadFileRes struct {
+type UploadLectureFileRes struct {
 	Result bool  `json:"result" dc:"is OK or not"`
 	Id     int64 `json:"id" dc:"file id"`
 }
 
-type CoverFileRes struct {
+type CoverLectureFileRes struct {
 	Result bool  `json:"result" dc:"is OK or not"`
 	Id     int64 `json:"id" dc:"file id"`
 }
 
-type DeleteFileRes struct {
+type DeleteLectureFileRes struct {
 	Result bool `json:"result" dc:"is OK or not"`
 }
