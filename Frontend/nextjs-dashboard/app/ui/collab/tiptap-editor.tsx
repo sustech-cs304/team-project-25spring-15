3dc31d874dc6c35cf1c8ade86e75afe94b133610
loaringlyc <3043119496@qq.com>
2025-05-05 23:20:43 +0800

    feat: collab editing using websocket server as backend to store data

--- Frontend/nextjs-dashboard/app/ui/collab/tiptap-editor.tsx
+++ Frontend/nextjs-dashboard/app/ui/collab/tiptap-editor.tsx
@@ -4,16 +4,22 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import { EditorContent, useEditor } from '@tiptap/react'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import Collaboration from '@tiptap/extension-collaboration'
 import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
 import Placeholder from '@tiptap/extension-placeholder'
 import * as Y from 'yjs'
-import { WebrtcProvider } from 'y-webrtc'
+// import { WebrtcProvider } from 'y-webrtc'
+import { WebsocketProvider } from 'y-websocket'
 
 const doc = new Y.Doc() // Initialize Y.Doc for shared editing
-const provider = new WebrtcProvider('asdhfjoifjoahsidf', doc);
+// const provider = new WebrtcProvider('my-roomname', doc);
+const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my-roomname', doc)
+
+wsProvider.on('status', event => {
+  console.log(event.status) // logs "connected" or "disconnected"
+})
 
 export default function TiptapEditor() {
   const editor = useEditor({
@@ -25,7 +31,7 @@ export default function TiptapEditor() {
         document: doc, // Configure Y.Doc for collaboration
       }),
       CollaborationCursor.configure({
-        provider,
+        provider: wsProvider,
         user: {
           name: 'Cyndi Lauper',
           color: '#f783ac',
