613c3e2edde5215234823d238d2620e43f38d29b
Zhu Yuchen <12211214@mail.sustech.edu.cn>
2025-05-13 00:17:44 +0800

    保留旧版login-form

--- Frontend/nextjs-dashboard/api.ts
+++ Frontend/nextjs-dashboard/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '/api',      // 会被 Next.js 转发到 47.117.144.50:8000/api
-  withCredentials: true // 如果你需要带 Cookie
+  baseURL: '/api',      // 用 Next.js 转发到 47.117.144.50:8000/api
+  withCredentials: true // 带 Cookie
 });
