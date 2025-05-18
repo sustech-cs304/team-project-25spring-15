3e73c6720e00b17422e595aac618b2b61c35ad72
wenxuan <deltaleung3@gmail.com>
2025-05-18 17:14:05 +0800

    add sharedfile related table into the initdabe.sql

--- Backend/intelligent-course-aware-ide/api/sharedFile/v1/sharedFile.go
+++ Backend/intelligent-course-aware-ide/api/sharedFile/v1/sharedFile.go
@@ -6,8 +6,8 @@ import (
 
 type CreateSharedFileReq struct {
 	g.Meta `path:"/api/sharedFile/create" method:"post" tags:"sharedFile" summary:"create a shared file task"`
-	FileId int64 `p:"fileId" v:"required"`
-	UserId int64 `p:"userId" v:"required"`
+	FileId int64 `p:"fileId" v:"required" dc:"A file which belongs to the user"`
+	UserId int64 `p:"userId" v:"required" dc:"The id of the user who want to upload the file as a shared file."`
 }
 
 type CreateSharedFileRes struct {
@@ -15,9 +15,9 @@ type CreateSharedFileRes struct {
 
 type UpdateSharedFileReq struct {
 	g.Meta       `path:"/api/sharedFile/update" method:"put" tags:"sharedFile" summary:"update a shared file task"`
-	FileId       int64  `p:"fileId" v:"required"`
-	UserId       int64  `p:"userId" v:"required"`
-	ChangeRecord string `p:"changeRecord" v:"required"`
+	FileId       int64  `p:"fileId" v:"required" dc:"The file to be changed"`
+	UserId       int64  `p:"userId" v:"required" dc:"The user who changes the file"`
+	ChangeRecord string `p:"changeRecord" v:"required" dc:"Record the change in specific format in the log"`
 }
 
 type UpdateSharedFileRes struct {
