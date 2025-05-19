d9abdbd5d86dbfd1b4bb8252b361a2fb871fad5a
wenxuan <deltaleung3@gmail.com>
2025-05-19 13:23:36 +0800

    add websocket.go to internal/service

--- Backend/intelligent-course-aware-ide/api/sharedFile/sharedFile.go
+++ Backend/intelligent-course-aware-ide/api/sharedFile/sharedFile.go
@@ -12,7 +12,9 @@ import (
 
 type ISharedFileV1 interface {
 	CreateSharedFile(ctx context.Context, req *v1.CreateSharedFileReq) (res *v1.CreateSharedFileRes, err error)
-	UpdateSharedFile(ctx context.Context, req *v1.UpdateSharedFileReq) (res *v1.UpdateSharedFileRes, err error)
 	SharedFileUserGetIn(ctx context.Context, req *v1.SharedFileUserGetInReq) (res *v1.SharedFileUserGetInRes, err error)
 	SharedFileUserGetOut(ctx context.Context, req *v1.SharedFileUserGetOutReq) (res *v1.SharedFileUserGetOutRes, err error)
+	UpdateSharedFile(ctx context.Context, req *v1.UpdateSharedFileReq) (res *v1.UpdateSharedFileRes, err error)
+	ConnectSharedFile(ctx context.Context, req *v1.ConnectSharedFileReq) (res *v1.ConnectSharedFileRes, err error)
+	GetSharedFileContent(ctx context.Context, req *v1.GetSharedFileContentReq) (res *v1.GetSharedFileContentRes, err error)
 }
--- Backend/intelligent-course-aware-ide/api/sharedFile/v1/sharedFile.go
+++ Backend/intelligent-course-aware-ide/api/sharedFile/v1/sharedFile.go
@@ -13,16 +13,6 @@ type CreateSharedFileReq struct {
 type CreateSharedFileRes struct {
 }
 
-type UpdateSharedFileReq struct {
-	g.Meta       `path:"/api/sharedFile/update" method:"put" tags:"sharedFile" summary:"update a shared file task"`
-	FileId       int64  `p:"fileId" v:"required" dc:"The file to be changed"`
-	UserId       int64  `p:"userId" v:"required" dc:"The user who changes the file"`
-	ChangeRecord string `p:"changeRecord" v:"required" dc:"Record the change in specific format in the log"`
-}
-
-type UpdateSharedFileRes struct {
-}
-
 type SharedFileUserGetInReq struct {
 	g.Meta `path:"/api/sharedFile/userGetIn" method:"post" tags:"sharedFile" summary:"user get in shared file"`
 	FileId int64 `p:"fileId" v:"required"`
@@ -39,4 +29,35 @@ type SharedFileUserGetOutReq struct {
 }
 
 type SharedFileUserGetOutRes struct {
+}
+
+// 更新共享文件请求结构
+type UpdateSharedFileReq struct {
+	g.Meta       `path:"/api/sharedFile/update" method:"put" tags:"sharedFile" summary:"update a shared file task"`
+	FileId       int64  `p:"fileId" v:"required" dc:"The file to be changed"`
+	UserId       int64  `p:"userId" v:"required" dc:"The user who changes the file"`
+	ChangeRecord string `p:"changeRecord" v:"required" dc:"Record the change in specific format"`
+}
+
+type UpdateSharedFileRes struct {
+}
+
+// WebSocket连接请求
+type ConnectSharedFileReq struct {
+	g.Meta `path:"/ws/sharedFile/connect" method:"get" tags:"sharedFile" summary:"connect to shared file editing"`
+	FileId int64 `p:"fileId" v:"required" dc:"The file to connect"`
+	UserId int64 `p:"userId" v:"required" dc:"The user who connects"`
+}
+
+type ConnectSharedFileRes struct {
+}
+
+// 获取文件内容及操作历史请求
+type GetSharedFileContentReq struct {
+	g.Meta `path:"/api/sharedFile/content" method:"get" tags:"sharedFile" summary:"get shared file content"`
+	FileId int64 `p:"fileId" v:"required" dc:"The file to get content for"`
+	UserId int64 `p:"userId" v:"required" dc:"The requesting user"`
+}
+
+type GetSharedFileContentRes struct {
 }
