58ca0dab5e0329e62f88296a1113d4bf23aa6cf0
loaringlyc <3043119496@qq.com>
2025-05-27 22:59:21 +0800

    fix: change the font to make it accessible for server

--- Frontend/nextjs-dashboard/app/ui/login/login-form.tsx
+++ Frontend/nextjs-dashboard/app/ui/login/login-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
   ExclamationCircleIcon,
@@ -22,7 +21,7 @@ export default function LoginForm() {
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+        <h1 className={`mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
