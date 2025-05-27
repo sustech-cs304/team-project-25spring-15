b472e4086cea72356631ae12b696f94513eae12c
wenxuan <deltaleung3@gmail.com>
2025-05-28 01:30:49 +0800

    refine comment's do and entity

---
+++ Backend/intelligent-course-aware-ide/internal/model/entity/comment.go
@@ -0,0 +1,16 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package entity
+
+// Comment is the golang structure for table Comment.
+type Comment struct {
+	CommentId          int64  `json:"commentId"          orm:"commentId"          description:""` //
+	RepliedToCommentId int64  `json:"repliedToCommentId" orm:"repliedToCommentId" description:""` //
+	LectureId          int    `json:"lectureId"          orm:"lectureId"          description:""` //
+	AuthorId           int64  `json:"authorId"           orm:"authorId"           description:""` //
+	Content            string `json:"content"            orm:"content"            description:""` //
+	CreateTime         string `json:"createTime"         orm:"createTime"         description:""` //
+	Likes              int64  `json:"likes"              orm:"likes"              description:""` //
+}
