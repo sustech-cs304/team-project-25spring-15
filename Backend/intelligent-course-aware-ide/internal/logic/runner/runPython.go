a82b64ffe06108f0c00980d97fd84bf77d7c631a
Y-1huadb <12211219@mail.sustech.edu.cn>
2025-05-28 12:05:26 +0800

    feat: save server.py

--- Backend/intelligent-course-aware-ide/internal/logic/runner/runPython.go
+++ Backend/intelligent-course-aware-ide/internal/logic/runner/runPython.go
@@ -3,12 +3,11 @@ package runner
 import (
 	"context"
 	v1 "intelligent-course-aware-ide/api/runner/v1"
-	"intelligent-course-aware-ide/internal/consts"
 	"os/exec"
 	"strings"
 )
 
-func (r *Runners) RunPythonCode(ctx context.Context, codeInfo *v1.RunnerReq, pathForPythonDocker string) (codeFeedback *v1.RunnerRes, err error) {
+func (r *Runners) RunPythonCode(ctx context.Context, codeInfo *v1.RunnerReq, dockerid string, pathForPythonDocker string) (codeFeedback *v1.RunnerRes, err error) {
 	// Here we use new path to replace the ordinary path
 	// But we have not implement file upload so we will implement it later
 	cmdStr := "python " + pathForPythonDocker
@@ -25,7 +24,7 @@ func (r *Runners) RunPythonCode(ctx context.Context, codeInfo *v1.RunnerReq, pat
 		}
 	}
 	var cmdContext []string = []string{
-		"exec", consts.TargetPythonDockerName, "bash", "-c", cmdStr,
+		"exec", dockerid, "bash", "-c", cmdStr,
 	}
 
 	var cmd *exec.Cmd = exec.CommandContext(ctx, "docker", cmdContext...)
